37d356c2da18675afe849cc3282be55d8c6b21ef
neburor <ruben.couoh@live.com.mx>
2015-08-11 20:54:52 -0500

    chat
    funciones

--- chat.php
+++ chat.php
@@ -21,15 +21,15 @@ class Chat
 		echo '<div class="collapse" id="allreply-chat">';
 		for ($i=$limit_comments; $i < mysql_num_rows($stream_comments) ; $i++) { 
 		$arreglo=mysql_fetch_row($stream_comments);
-		$time = strtotime($arreglo[3]); 
+		$time = strtotime($arreglo[4]); 
 		$date = date("d/m/y", $time);
-			if($arreglo[1]=="contact"){
-				echo '<p class="text-left"><i class="fa fa-user icon"></i> <span class="comment-date pull-right">'.$date.'</span><br>'.$arreglo[4].'</p>';
+			if($arreglo[2]=="contact"){
+				echo '<p class="text-left"><i class="fa fa-user icon"></i> <span class="comment-date pull-right">'.$date.'</span><br>'.$arreglo[5].'</p>';
 			}
-			if($arreglo[1]=="reply"){
+			if($arreglo[2]=="reply"){
 				echo '<p class="text-right"><span class="comment-date pull-left">'.$date.'</span> ';
 				if($icon_logo){ echo '<img src="'.$icon_logo.'" class="icon"/>'; } else { echo '<i class="fa fa-user icon"></i>'; }
-				echo '<br>'.$arreglo[4].'</p>';
+				echo '<br>'.$arreglo[5].'</p>';
 			}
 		}
 		echo '</div>';
@@ -38,15 +38,15 @@ class Chat
 		for ($i=0; $i < mysql_num_rows($stream_comments) ; $i++) { 
 		$arreglo=mysql_fetch_row($stream_comments);
 
-		$time = strtotime($arreglo[3]); 
+		$time = strtotime($arreglo[4]); 
 		$date = date("d/m/y", $time);
-			if($arreglo[1]=="contact"){
-				echo '<p class="text-left"><i class="fa fa-user icon"></i> <span class="comment-date pull-right">'.$date.'</span><br>'.$arreglo[4].'</p>';
+			if($arreglo[2]=="contact"){
+				echo '<p class="text-left"><i class="fa fa-user icon"></i> <span class="comment-date pull-right">'.$date.'</span><br>'.$arreglo[5].'</p>';
 			}
-			if($arreglo[1]=="reply"){
+			if($arreglo[2]=="reply"){
 				echo '<p class="text-right"><span class="comment-date pull-left">'.$date.'</span> ';
 				if($icon_logo){ echo '<img src="'.$icon_logo.'" class="icon"/>'; } else { echo '<i class="fa fa-user icon"></i>'; }
-				echo '<br>'.$arreglo[4].'</p>';
+				echo '<br>'.$arreglo[5].'</p>';
 			}
 
 		}
@@ -63,22 +63,22 @@ class Chat
 		if ($show_message=='show'){ echo '<div class="result"><div class="col-xs-12"><p><i class="fa fa-info-circle"></i> La respuesta a su mensaje la podra consultar posteriormente ingresando de nuevo a esta pagina.</p></div></div>';} else {echo '<div class="result"></div>';}
 		if($form_type=='contact'){
 			echo '<div class="form-group"><div class="input-group"><span class="input-group-addon"><i class="fa fa-comment"></i></span>
-              <textarea name="mensaje" placeholder="Su mensaje a la administración ..." class="form-control" required></textarea></div></div>';
+              <textarea name="message" placeholder="Su mensaje a la administración ..." class="form-control" maxlength="1024" minlength="5" required></textarea></div></div>';
 		}
 		if($form_type=='chat'){
-		echo '<div class="form-group"><div class="input-group"><input type="text" name="mensaje" placeholder="Su mensaje a la administración ..." class="form-control" required></input><div class="input-group-btn"><button type="submit" class="btn btn-default"><i class="fa fa-share"></i></button></div></div></div>';
+		echo '<div class="form-group"><div class="input-group"><input type="text" name="message" placeholder="Su mensaje a la administración ..." class="form-control" maxlength="1024" minlength="5" required></input><div class="input-group-btn"><button type="submit" class="btn btn-default"><i class="fa fa-share"></i></button></div></div></div>';
 		}
-		if ($opc_email=='show'){ echo '<label class="col-xs-12"><input type="checkbox" name="opciones"><span>Notificarme a mi correo.</span></label>';
+		if ($opc_email=='show'){ echo '<label class="col-xs-12"><input type="checkbox" name="options"><span>Notificarme a mi correo.</span></label>';
 		echo '<div class="collapse col-xs-12" aria-expanded="false">
             <div class="form-group">
             <div class="input-group">
               <span class="input-group-addon"><i class="fa fa-user"></i></span>
-              <input type="email" name="correo" class="form-control" placeholder="correo@ejemplo.com" disabled="disabled">
+              <input type="email" name="email" class="form-control" placeholder="correo@ejemplo.com" disabled="disabled" required>
             </div>
             </div>
           </div>';}
 		if($form_type=="contact") {
-          	echo '<div class="form-group col-xs-12"><button type="submit" class="btn btn-default btn-block">Enviar mensaje <i class="fa fa-share"></i></button></div>';
+          	echo '<div class="form-group"><button type="submit" class="btn btn-default btn-block">Enviar mensaje <i class="fa fa-share"></i></button></div>';
           }
           
         echo '</form>';
---
+++ code.js
@@ -0,0 +1,119 @@
+$(document).ready(function () {
+
+	//Activar funciones de chat
+	$('.wvt-chat form').wvt_chat();
+});
+
+
+$.fn.wvt_chat = function() {
+	$(this).each(function (){
+	//Agregar novalidate
+	$(this).attr('novalidate','novalidate');
+	//Buscar y Activar/desactivar opciones adicionales
+	form_opc($(this).find('input[name="options"]'));
+	});	
+	
+	//Cuando se envia el formulario
+	$(this).on("submit", function(e) {
+	event.preventDefault(e);
+	btncontrol=$(this).find('button[type="submit"]');
+	result=$(this).find('div.result');
+    $(result).empty();
+
+    //Verificar los inputs
+    if(!verifyInputs($(this).find('input,textarea,select'),result))
+    {
+	//Se crea el formulario y agregan elementos de ser necesario para enviar
+	var chat_messages = new FormData($(this)[0]);
+	chat_messages.append("type", $(this).attr('form-type'));	
+	
+	//Se procesa el envio
+	form_send(chat_messages,inputs,result,btncontrol);
+	}
+
+	});
+}
+
+function form_send(form,inputs,result,btncontrol){
+	  //Envio del formulario con AJAX
+        $.ajax({
+            type: "POST",
+            url: "processData.php",
+            dataType: "json",
+            data: form,
+            cache: false,
+            contentType: false,
+            processData: false, 
+            complete: function(data){
+            },
+            success: function(data){  
+            },
+            beforeSend: function(){ 
+            	beforesendAJAX(inputs,result,btncontrol);
+            },
+            error: function(){ 
+            	beforesendAJAX(inputs,result,btncontrol);
+            //	errorAJAX(result,btncontrol); 
+            }
+         });
+}
+
+function errorAJAX(result,btncontrol){
+    $(result).append('<div class="col-xs-12"><div class="alert alert-error alert-dismissible fade in" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span></button><i class="fa fa-warning"></i> <b>Error !</b>, no se puede conectar al servidor, si persiste intente mas tarde.</div></div>'); 
+    $(btncontrol).removeAttr("disabled");
+    $(btncontrol).empty().append('<i class="fa fa-warning"> Reintentar');  
+}
+
+function form_opc (opciones) {
+	
+$(opciones).on('click', function() {
+    collapse=$(opciones).parents('form').find('div.collapse');
+    inputs=$(collapse).find('input,textarea,select');
+    
+ if($(opciones).is(':checked')){
+    $(inputs).each(function(){
+        $(this).removeAttr('disabled');
+    });
+    $(collapse).collapse('toggle');
+ }
+ else{
+    $(inputs).each(function(){
+        $(this).attr('disabled','disabled');
+    });
+    $(collapse).collapse('toggle');
+ }
+});
+}
+function verifyInputs (inputs,result){
+	var error=0;
+	//Comprobar los inputs a enviar y si son correctos
+	$(inputs).each(function(){
+		if($(this).is(':disabled')){}
+        else {
+            if($(this).attr('name')!='options' && $(this).attr('name')!='autopass'){
+
+            	if(!checkINPUT($(this))){
+
+            	}
+            	else {
+            		$(this).addClass('inp_error');
+            	error++;
+            	}
+            }
+        }
+
+	});
+	console.log(error);
+	  return error
+}
+
+function checkINPUT(input){
+	var error = 0;
+	if($(input).is(':required')){
+		if($(input).val()==""){error++;}
+	}
+	if($(input).attr("type")=="email"){ 
+        if(!/^[\w.-]+@[\w.-]+\.[a-zA-Z]{2,6}$/.test($(input).val())){ error++; }
+    }
+    return error
+}
