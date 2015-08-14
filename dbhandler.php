6feb74074929e31c5ef437cdf6bcd8d0a99f3467
neburor <ruben.couoh@live.com.mx>
2015-08-13 22:56:55 -0500

    dbhandler
    post

--- dbhandler.php
+++ dbhandler.php
@@ -10,7 +10,9 @@ class API {
 	
     $params = array_merge(array(
         'domain' => null,
+        'type' => null,
         'id-profile' => null,
+        'message' => null,
         'order-by' => 'id',
         'order' => 'ASC'
     	), $params);
@@ -19,13 +21,32 @@ class API {
     	case 'get':
     		$sql=mysql_query("SELECT * FROM `chat` WHERE `domain` = '".$params['domain']."' AND `id_profile` = '".$params['id-profile']."' ORDER BY `".$params['order-by']."` ".$params['order']."");
 
-			$json = array();
+			$response = array();
 			while($i = mysql_fetch_assoc($sql)) {
-    			$json[] = $i;
+    			$response[] = $i;
 				}
-				return json_encode($json);
+				return json_encode($response);
     		break;
-    	
+
+    	case 'post':
+    		if(mysql_query("INSERT INTO `chat` (`id`, `domain`, `type`, `id-profile`, `date`, `message`) VALUES (NULL, '".$params['domain']."', '".$params['type']."', '".$params['id-profile']."', '".date("Y-m-d H:i:s")."', '".$params['message']."' )")) {	
+				$id = mysql_insert_id();
+				$sql=mysql_query("SELECT * FROM `chat` WHERE `id` = '".$id."' LIMIT 1");
+				$response = array();
+				while($i = mysql_fetch_assoc($sql)) {
+    				$response[] = $i;
+				}
+				array_push($response, "status", "correct");
+				return json_encode($response);
+				break;
+			}
+			else {
+				$response = ['status'=>'error'];
+				return json_encode($response);
+			}
+
+    		break;
+
     	default:
     		# code...
     		break;
